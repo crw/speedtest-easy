c6fdf1020c2f6b6d2b813ca4ca16b1ee7f10e14e
Craig Wright <craig@wright.name>
2016-01-27 21:47:36 -0800

    Large commit, major changes to the web side.

--- speedtest-web/public/speedtest-web.js
+++ speedtest-web/public/speedtest-web.js
@@ -1,11 +1,62 @@
 
+
+function median(values) {
+  // @see https://gist.github.com/caseyjustus/1166258
+  values.sort( function(a,b) {return a - b;} );
+
+  var half = Math.floor(values.length/2);
+
+  if(values.length % 2)
+    return values[half];
+  else
+    return (values[half-1] + values[half]) / 2.0;
+}
+
+
+function generateMonthValues() {
+
+  function successfn(data) {
+    var options = [],
+        date = new Date(data.start),
+        monthName = {
+          0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun',
+          6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec'
+        };
+
+    today = new Date();
+    for (year = date.getFullYear(); year <= today.getFullYear(); year++) {
+      month = (year === date.getFullYear()) ? date.getMonth() : 0;
+      while ( month < 12 && !(year === today.getFullYear() && month > today.getMonth()) ) {
+        options.push(year + ' ' + monthName[month]);
+        month++
+      }
+    }
+    $.each(options, function(key, value) {
+       $('#specificity')
+          .append($('<option>', { value : value })
+          .text(value));
+    });
+  }
+
+  $.ajax({
+    url: '/api/first',
+    dataType: 'json',
+    type: 'GET',
+    success: successfn.bind(this),
+    error: function(xhr, status, err) {
+      console.error('/api/first', status, err.toString());
+    }.bind(this)
+  });
+}
+
+
 function decorateToolTip(meta, value) {
 
   var output = "";
 
   value = (value == 0) ? "0" : value;
 
-  output = output + "<span class='tooltip-value'>" + value + "kbps</span>";
+  output = output + "<span class='tooltip-value'>" + value + "Mbps</span>";
   meta = Chartist.deserialize(meta);
   for (i = 0; i < meta.length; i++) {
     output = output + "<p><span class='tooltip-meta'>" + meta[i] + "</span></p>";
@@ -16,44 +67,61 @@ function decorateToolTip(meta, value) {
 
 function drawChart(start, end) {
 
-  var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-      [
-        {
-          meta: ['Comcast 25.6ms 15.5 miles'],
-          value: 5
-        }, {
-          meta: ['Comcast 25.6ms 15.5 miles'],
-          value: 2
-        }, {
-          meta: ['Comcast 25.6ms 15.5 miles'],
-          value: 4
-        }, {
-          meta: ['Comcast 25.6ms 15.5 miles'],
-          value: 2
-        }, {
-          meta: ['Comcast 25.6ms 15.5 miles'],
-          value: 0
-        }
-      ]
-    ]
+  var data_uri = '/api/all';
+
+  var successfn = function (data) {
+    var spddata = { labels: [], series: [[], []] };
+
+    data.map( function(obj) {
+      spddata.labels.push(new Date(obj.start).getHours());
+      spddata.series[0].push({
+        meta: [ obj.hostname, obj.distance, obj.latency],
+        value: parseInt(obj.down) / 1000
+      });
+      spddata.series[1].push({
+        meta: [ obj.hostname, obj.distance, obj.latency],
+        value: parseInt(obj.up) / 1000
+      });
+    });
+    if (spddata.labels.length > 100) {
+      spddata.labels = spddata.labels.slice(-30, -1);
+      spddata.series[0] = spddata.series[0].slice(-31, -1);
+      spddata.series[1] = spddata.series[1].slice(-31, -1);
+    }
+    console.log(spddata);
+    new Chartist.Line('.ct-chart', spddata, {
+        plugins: [
+            Chartist.plugins.tooltip({
+                tooltipFnc: decorateToolTip
+            }),
+            Chartist.plugins.ctPointLabels({
+              textAnchor: 'middle'
+            })
+        ]
+    });
   };
 
-  // Create a new line chart object where as first parameter we pass in a selector
-  // that is resolving to our chart container element. The Second parameter
-  // is the actual data object.
-  new Chartist.Line('.ct-chart', data, {
-      plugins: [
-          Chartist.plugins.tooltip({
-              tooltipFnc: decorateToolTip
-          })
-      ]
+  $.ajax({
+    url: data_uri,
+    dataType: 'json',
+    type: 'GET',
+    success: successfn.bind(this),
+    error: function(xhr, status, err) {
+      console.error(data_uri, status, err.toString());
+    }.bind(this)
   });
+
 }
 
 
-
-$( document ).ready(drawChart('a', 'b'));
+$( document ).ready(function () {
+  generateMonthValues();
+  drawChart('a', 'b');
+  $('#granularity').change(function() {
+    if ($('#granularity').val() === 'day') {
+      $('#day').css('display', 'inline');
+    } else {
+      $('#day').css('display', 'none');
+    }
+  });
+});
--- speedtest-web/speedtest-web.js
+++ speedtest-web/speedtest-web.js
@@ -1,14 +1,46 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var speedtestSqlite = require('./model/speedtest-sqlite.js');
+
+
+var port = process.env.PORT || 3000;
+var filename = process.env.DB_FILENAME || '../speedtest.sqlite';
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-app.get('/api/range', function (req, res) {
-  res.send('Hello World!');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.json({'message': 'Hello World!'});
 });
 
-app.listen(3000, function () {
-  console.log('speeedtest-web listening on port 3000!');
-})
 
+router.get('/first', function (req, res) {
+  sql = new speedtestSqlite(filename);
+  sql.first(function (err, row) {
+    res.json(row);
+  });
+});
+
+
+router.get('/range', function (req, res) {
+  res.json({'message': 'Hello World!'});
+});
+
+router.get('/all', function (req, res) {
+  sql = new speedtestSqlite(filename);
+  sql.all(function (err, rows) {
+    res.json(rows);
+  });
+});
+
+
+
+app.use('/api', router);
+
+app.listen(port, function () {
+  console.log('speeedtest-web listening on port ' + port + '!');
+})
