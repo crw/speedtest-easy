871444f52431f477df5128ef0eba6740e6cfeca9
Craig Wright <craig@wright.name>
2016-01-27 21:49:34 -0800

    Database model for Node.js, first commit.
    Taking a very specific use-case oriented approach to
    this interface. This is fine for this specific little
    app but I wouldn't call it well-thought-out.

---
+++ speedtest-web/model/speedtest-sqlite.js
@@ -0,0 +1,35 @@
+var sqlite3 = require('sqlite3').verbose();
+
+
+function speedtest_sqlite (filename) {
+	this.db = new sqlite3.Database(filename);
+}
+
+speedtest_sqlite.prototype.range = function(callback, start, end) {
+
+	var stmt = 'SELECT * FROM speedtest WHERE start >= ? AND end <= ?';
+	var query;
+
+	query = this.db.prepare(stmt, start, end);
+	query.all(callback);
+}
+
+speedtest_sqlite.prototype.first = function(callback) {
+
+	var stmt = 'SELECT * FROM speedtest LIMIT 1';
+	this.db.get(stmt, callback);
+}
+
+speedtest_sqlite.prototype.all = function(callback) {
+
+	var stmt = 'SELECT * FROM speedtest';
+	this.db.all(stmt, callback);
+}
+
+speedtest_sqlite.prototype.count = function(callback) {
+
+	var stmt = 'SELECT COUNT(*) as count FROM speedtest';
+	this.db.all(stmt, callback);
+}
+
+module.exports = speedtest_sqlite;
