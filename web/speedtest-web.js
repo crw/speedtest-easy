04821e9df6d25de8940db212695f824c47306a01
Craig Wright <craig@wright.name>
2016-02-10 13:11:43 -0800

    Adding some example files for testing.

--- web/speedtest-web.js
+++ web/speedtest-web.js
@@ -16,7 +16,7 @@ var SpeedtestSqlite = require('./model/speedtest-sqlite.js');
 
 
 var port = process.env.PORT || 3000;
-var filename = process.env.DB_FILENAME || 'speedtest.sqlite';
+var filename = process.env.DB_FILENAME || '../example/speedtest.sqlite';
 
 var sql = new SpeedtestSqlite(filename);
 
